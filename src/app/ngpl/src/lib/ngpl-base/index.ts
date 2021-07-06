27642f26ec01923ec634f410676aa47829b05605
Yoan Asdrubal <yoan.asdrubal.kubano@gmail.com>
2021-07-06 17:30:44 -0400

    Refactor to use ngpl-filter

--- src/app/ngpl/src/lib/ngpl-base/index.ts
+++ src/app/ngpl/src/lib/ngpl-base/index.ts
@@ -1,10 +1,2 @@
 
-export * from './injection-tokens/ngpl-filter-base';
-
-export * from './injection-tokens/ngpl-datatable-base';
-
-export * from './injection-tokens/ngpl-filter-menu-base';
-
-export * from './injection-tokens/ngpl-filter-applied-base';
-
 export * from './selection';
--- src/app/ngpl/src/lib/ngpl-base/injection-tokens/ngpl-filter-applied-base.ts
+++
@@ -1,8 +0,0 @@
-import {NgplFilterConfigDirective} from '../../ngpl-filter/ngpl-filter-config.directive';
-import {InjectionToken} from '@angular/core';
-
-export interface NgplFilterAppliedBase {
-  registerFilter: (filterConfig: NgplFilterConfigDirective) => void;
-}
-
-export const NGPL_FILTER_APPLIED_BASE = new InjectionToken<NgplFilterAppliedBase>('NgplFilterAppliedBase');
